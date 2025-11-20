f5f8a3c864617c107393ee696e78abc3bad13975
kuruv <veerumails938@gmail.com>
2025-11-20 22:42:14 +0530

    Checked palindrome change 3 added Null checks for the given input

--- src/main/java/org/example/string/pgms/Palindrome.java
+++ src/main/java/org/example/string/pgms/Palindrome.java
@@ -5,26 +5,32 @@ public class Palindrome
     public static String PalindromeCheck(String str)
     {
         String s = "";
-        for(int i=str.length()-1; i>=0; i--)
-        {
-            s = s.concat(String.valueOf(str.charAt(i)));
+
+        if(str!=null && !str.isBlank()){
+
+            for(int i=str.length()-1; i>=0; i--)
+            {
+                s = s.concat(String.valueOf(str.charAt(i)));
+            }
+            System.out.println("s value    :: "+s);
+            if(s.equalsIgnoreCase(str))
+            {
+                return "Its Palindrome";
+            }
+            else
+            {
+                return "Its not a Palindrome";
+            }
         }
 
-        System.out.println("s value    :: "+s);
-        if(s.equalsIgnoreCase(str))
-        {
-            return "Its Palindrome";
-        }
-        else
-        {
-            return "Its not a Palindrome";
-        }
+        return null;
+
     }
 
 
     public static void main(String[] args)
     {
-        String result = PalindromeCheck("java");
+        String result = PalindromeCheck("madam");
         System.out.println(result);
 
     }
