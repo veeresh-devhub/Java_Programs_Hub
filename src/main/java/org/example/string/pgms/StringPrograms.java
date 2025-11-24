b36d96b25548266f2af9ce5c3cd89cc084b4c92e
kuruv <veerumails938@gmail.com>
2025-11-24 11:38:12 +0530

    Checked palindrome change 3 added Null checks for the given input

--- src/main/java/org/example/string/pgms/StringPrograms.java
+++ src/main/java/org/example/string/pgms/StringPrograms.java
@@ -5,5 +5,6 @@ public class StringPrograms {
     public static void main(String[] args) {
         System.out.println("String Programs:");
         System.out.println("Hi");
+        System.out.println("Today is Monday");
     }
 }
