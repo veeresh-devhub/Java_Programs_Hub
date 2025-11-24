1a50a6448bd8a89e777982b1ac015a21de85f09b
kuruv <veerumails938@gmail.com>
2025-11-24 12:48:17 +0530

    Found length

---
+++ src/main/java/org/example/string/pgms/FindLength.java
@@ -0,0 +1,15 @@
+package org.example.string.pgms;
+
+public class FindLength
+{
+    public static int Duplicates(String str)
+{
+
+    return str.length();
+}
+    public static void main(String[] args)
+    {
+       int res = Duplicates("Veeresh1");
+        System.out.println(res);
+    }
+}
